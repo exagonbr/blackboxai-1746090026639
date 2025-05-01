da751ce27df8c1beadd1b4da5337c339ebdde11d
Estevão Maia <estevao@programmer.net>
2025-05-01 07:04:58 -0300

    Add client/tailwind.config.js 1746093897692

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: '#3B82F6',
+          600: '#2563EB',
+          700: '#1D4ED8'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
