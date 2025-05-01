05559983a57e7968e4c7433241c7923b1f69817a
Estevão Maia <estevao@programmer.net>
2025-05-01 07:05:04 -0300

    Add migrations/20230501-create-alerts.js 1746093903695

---
+++ migrations/20230501-create-alerts.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Alerts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      faceId: {
+        type: Sequelize.STRING
+      },
+      cameraId: {
+        type: Sequelize.STRING
+      },
+      alertType: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Alerts');
+  }
+};
